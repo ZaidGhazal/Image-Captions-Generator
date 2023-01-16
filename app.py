dd422351047583b283402c3022a7ceb7267170da
Zaid Ghazal <zghazal@beyond.ai>
2023-01-16 03:24:24 +0300

    Add tabs to app

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
 def run_app():
-    st.title("My Streamlit App")
+    st.title("Images Caption Generator")
 
-    if st.checkbox('Show Hello World'):
-        st.write('Hello, World!')
+    tab1, tab2, tab3 = st.tabs(["Home", "Train", "Inference"])
+
+    with tab1:
+        st.write("Home")
+    
+    with tab2:
+        st.write("Here you can train a new neural netowrk model. You should specifiy values to the following paramters and hit the trian button to start.")
+        
+    
+    with tab3:
+        st.write("Here you can generate a caption for an image. You should select an image and hit the generate button to start.")
+        
 
 if __name__ == "__main__":
     run_app()
