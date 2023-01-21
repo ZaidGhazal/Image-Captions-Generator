0c75d52f4bffb6f9001e597a3a252f11f5ecbed5
Zaid Ghazal <zghazal@beyond.ai>
2023-01-21 15:20:16 +0300

    Edit argument type in main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import argparse
 
 # get cli parameters
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--disable_training', type=bool, default=True, required=False,
+parser.add_argument('--disable_training', type=int, default=1, required=False,
                     help='Boolean, whether to diable training app or not')
+
 if __name__ == "__main__":
     args = parser.parse_args()
-    disable_training = args.disable_training
-    print("disable training: ", disable_training)
+    disable_training = True if args.disable_training else False
     run_app(disable_training=disable_training)
