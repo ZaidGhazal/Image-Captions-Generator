6a082cc0c9919a67429343c75613a94c43ca46a3
Zaid <zaid.ghazal20@gmail.com>
2023-01-23 15:11:12 +0300

    Add CUDA_FOUND flag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app import run_app
 import argparse
+import torch
 
 # get cli parameters
 parser = argparse.ArgumentParser(description='')
@@ -9,4 +10,5 @@ parser.add_argument('--disable_training', type=int, default=1, required=False,
 if __name__ == "__main__":
     args = parser.parse_args()
     disable_training = True if args.disable_training else False
+    print("CUDA Found: ", torch.cuda.is_available())
     run_app(disable_training=disable_training)
